0eb6662e15a7f81dd49da4e2337eb7e860f8ce16
AbderrahmaneMM <abdomebani28@gmail.com>
2025-03-10 16:23:23 +0100

    save image in maui

--- Moussadjal mobile app/MainPage.xaml.cs
+++ Moussadjal mobile app/MainPage.xaml.cs
@@ -2,6 +2,14 @@
 using ZXing.Net;
 using System.Data.SqlClient;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using Microsoft.Maui.Controls;
+using ZXing.Common;
+using ZXing;
+using SkiaSharp;
+using ZXing.PDF417.Internal;
+
+
+
 namespace Moussadjal_mobile_app
 {
     public partial class MainPage : ContentPage
@@ -10,46 +18,41 @@ namespace Moussadjal_mobile_app
         {
             InitializeComponent();
         }
-        //byte[] convertImageToByte(Image img)
-        //{
-        //    using (MemoryStream ms = new MemoryStream())
-        //    {
-        //        img.Save(ms, ImageFormat.Png);
-        //        return ms.ToArray();
-        //    }
-
-        //}
-        byte[] convertImageToByte(Image img)
+        byte[] convertToByte(Image img)
         {
             using (MemoryStream ms = new MemoryStream())
             {
-                //img.
-                //img.Save(ms, ImageFormat.Png);
+               // img.Save(ms, ImageFormat.Png);
                 return ms.ToArray();
             }
 
         }
-        private void GenerateButton_Clicked(object sender, EventArgs e)
+        private async Task<byte[]> ConvertImageToByteArray(ImageSource imageSource)
+        {
+            if (imageSource is StreamImageSource streamImageSource)
+            {
+                using (Stream stream = await streamImageSource.Stream(CancellationToken.None))
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        await stream.CopyToAsync(ms);
+                        return ms.ToArray();
+                    }
+                }
+            }
+            return null;
+        }
+
+        private async void GenerateButton_Clicked(object sender, EventArgs e)
         {
             c_central db = new c_central();
-
-            bgv.Value = $"Numero sequential: {SequentialEntry.Text}\nNumero d'inventaire: {InventoryEntry.Text}\nLieu: {PlaceEntry.Text}";
-            BarcodeImage.Source = bgv.Value;
-
-            byte[] img; //= convertImageToByte(BarcodeImage);
-           // db.FillscdToInsert("INSERT INTO Bien (numero_dinventaire, numero_sequentiel, id_lieu, datamatrix_code) VALUES ('"+ int.Parse(InventoryEntry.Text) + "', '"+ int.Parse(SequentialEntry.Text) + "', '"+ PlaceEntry.Text + "', '"+img+"')");
-           /*SqlConnection connection = new SqlConnection();
-            connection.ConnectionString = @"Data Source=sql.bsite.net\MSSQL2016;Initial Catalog=abdomm_Moussadjalsql;User ID=abdomm_Moussadjalsql;Password=9876543210987";
-            connection.Open(); 
-            SqlCommand cmd = new SqlCommand("INSERT INTO Bien (numero_dinventaire, numero_sequentiel, id_lieu, datamatrix_code) VALUES (@numero_dinventaire, @numero_sequentiel, @id_lieu, @datamatrix_code)");
-            cmd.CommandType = CommandType.Text;
-            cmd.Connection = connection;
-            cmd.Parameters.AddWithValue("@numero_dinventaire", );
-            cmd.Parameters.AddWithValue("@numero_sequentiel",  );
-            cmd.Parameters.AddWithValue("@id_lieu", );
-            cmd.Parameters.AddWithValue("@datamatrix_code", bgv.Value);
-            cmd.ExecuteNonQuery();
-            connection.Close();*/
+            //desplay the barcode 
+             bgv.Value = $"Numero sequential: {SequentialEntry.Text}\nNumero d'inventaire: {InventoryEntry.Text}\nLieu: {PlaceEntry.Text}";
+             BarcodeImage.Source = bgv.Value;
+            //convert image to byte array 
+            byte[] img = await ConvertImageToByteArray(BarcodeImage.Source);
+            //save in database 
+            db.FillscdToInsert("INSERT INTO Bien (numero_dinventaire, numero_sequentiel, id_lieu, datamatrix_code) VALUES ('"+ int.Parse(InventoryEntry.Text) + "', '"+ int.Parse(SequentialEntry.Text) + "', '"+ PlaceEntry.Text + "', '"+img+"')");
             DisplayAlert("add secsses", InventoryEntry.Text + SequentialEntry.Text, "ok");
 
         }
