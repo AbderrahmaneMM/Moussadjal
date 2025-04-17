1db03ea0e29f2c5319da3224ef9aae19a48340b4
samibennoui <sami.bennoui.43@gmail.com>
2025-04-17 19:15:10 +0100

    Microsoft.data.SqlClient

--- Moussadjal mobile app/c_central.cs
+++ Moussadjal mobile app/c_central.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+//using System.Data.SqlClient;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
 
 
 namespace Moussadjal_mobile_app
