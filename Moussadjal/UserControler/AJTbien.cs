4eb292a31fb11f602479b1ad8848a0716fc4c529
AbderrahmaneMM <abdomebani28@gmail.com>
2025-05-13 22:23:12 +0100

    Ajtbien fix

--- Moussadjal/UserControler/AJTbien.cs
+++ Moussadjal/UserControler/AJTbien.cs
@@ -40,36 +40,41 @@ namespace Moussadjal.UserControler
 
         private void Ajtbtn_Click(object sender, EventArgs e)
         {
+
             try
             {
-                    //generation datamatrix barcode
-                    var barcodeWriter = new BarcodeWriter
-                    {
-                        Format = BarcodeFormat.DATA_MATRIX,
-                        Options = new EncodingOptions
-                        {
-                            Height = 300,
-                            Width = 300,
-                            Margin = 10
-                        },
-                        Renderer = new BitmapRenderer()
-                    };
-                    string NumIn = (db.FillscdToSelectCount("SELECT COUNT(*) FROM Bien")+1).ToString();
-                    Bitmap barcodeBitmap = barcodeWriter.Write($"{NumIn}/{NsComboBox.Text}/{LieuComboBox.Text}");
-                    BarcodPicture.Image = barcodeBitmap;
-                    //convert image barcode to byte array
-                    byte[] img = convertImageToByte(BarcodPicture.Image);
-                    //insert 'bien' to db
-                    db.Ajouter("INSERT INTO Bien (numero_dinventaire, numero_sequentiel, id_lieu, datamatrix_code) VALUES ((SELECT ISNULL(MAX(numero_dinventaire), 0) + 1 FROM Bien), '" + Convert.ToInt32(NsComboBox.SelectedValue) + "', '" + LieuComboBox.SelectedValue + "', '" + img + "')");
-                     db.Ajouter("UPDATE Description_de_bien SET quantite = quantite + 1 WHERE numero_sequentiel = '" + Convert.ToInt32(NsComboBox.SelectedValue) + "' ");
-                    MessageBox.Show("add secsses", NumIn, MessageBoxButtons.OK, MessageBoxIcon.Information);
-              
+                //generation datamatrix barcode
+                var barcodeWriter = new BarcodeWriter
+            {
+                Format = BarcodeFormat.DATA_MATRIX,
+                Options = new EncodingOptions
+                {
+                    Height = 300,
+                    Width = 300,
+                    Margin = 10,
+                },
+                Renderer = new BitmapRenderer()
+            };
+            string NumIn = (db.FillscdToSelectCount("SELECT COUNT(*) FROM Bien") + 1).ToString();
+            string original = $"{NumIn}/{NsComboBox.Text}/{LieuComboBox.Text}";
+            string base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(original));
+            Bitmap barcodeBitmap = barcodeWriter.Write(base64);
+            BarcodPicture.Image = barcodeBitmap;
+
+            //convert image barcode to byte array
+            byte[] img = convertImageToByte(BarcodPicture.Image);
+            //insert 'bien' to db
+            db.Ajouter("INSERT INTO Bien (numero_dinventaire, numero_sequentiel, id_lieu, datamatrix_code) VALUES ((SELECT ISNULL(MAX(numero_dinventaire), 0) + 1 FROM Bien), '" + Convert.ToInt32(NsComboBox.SelectedValue) + "', '" + LieuComboBox.SelectedValue + "', '" + img + "')");
+            db.Ajouter("UPDATE Description_de_bien SET quantite = quantite + 1 WHERE numero_sequentiel = '" + Convert.ToInt32(NsComboBox.SelectedValue) + "' ");
+            MessageBox.Show("add secsses", NumIn, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-        }
+
+         }
 
         private void mailtxtboxkey(object sender, KeyEventArgs e)
         {
---
+++ Moussadjal/UserControler/Etiquette .Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Moussadjal.UserControler
+{
+    partial class Etiquette
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Etiquette));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2VSeparator2 = new Guna.UI2.WinForms.Guna2VSeparator();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LieuComboBox = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.DescreptionComboBox = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.NumeroInventaireCB = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(3, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(296, 390);
+            this.panel1.TabIndex = 0;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.BorderRadius = 16;
+            this.guna2Button1.CheckedState.FillColor = System.Drawing.Color.White;
+            this.guna2Button1.CheckedState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(216)))));
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(216)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(3, 14);
+            this.guna2Button1.Margin = new System.Windows.Forms.Padding(0);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(58, 56);
+            this.guna2Button1.TabIndex = 30;
+            this.guna2Button1.Text = "Print";
+            this.guna2Button1.UseTransparentBackground = true;
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2VSeparator2
+            // 
+            this.guna2VSeparator2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(216)))));
+            this.guna2VSeparator2.Location = new System.Drawing.Point(305, 97);
+            this.guna2VSeparator2.Name = "guna2VSeparator2";
+            this.guna2VSeparator2.Size = new System.Drawing.Size(27, 389);
+            this.guna2VSeparator2.TabIndex = 31;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(14, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 22);
+            this.label4.TabIndex = 44;
+            this.label4.Text = "INSFP boussmail";
+            // 
+            // LieuComboBox
+            // 
+            this.LieuComboBox.AutoRoundedCorners = true;
+            this.LieuComboBox.BackColor = System.Drawing.Color.Transparent;
+            this.LieuComboBox.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(216)))));
+            this.LieuComboBox.BorderRadius = 17;
+            this.LieuComboBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.LieuComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.LieuComboBox.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.LieuComboBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.LieuComboBox.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.LieuComboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.LieuComboBox.ItemHeight = 30;
+            this.LieuComboBox.Location = new System.Drawing.Point(338, 108);
+            this.LieuComboBox.Name = "LieuComboBox";
+            this.LieuComboBox.Size = new System.Drawing.Size(272, 36);
+            this.LieuComboBox.TabIndex = 32;
+            this.LieuComboBox.SelectedIndexChanged += new System.EventHandler(this.LieuComboBox_SelectedIndexChanged);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.BorderRadius = 16;
+            this.guna2Button2.CheckedState.FillColor = System.Drawing.Color.White;
+            this.guna2Button2.CheckedState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(216)))));
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(216)))));
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.Location = new System.Drawing.Point(338, 451);
+            this.guna2Button2.Margin = new System.Windows.Forms.Padding(0);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(272, 35);
+            this.guna2Button2.TabIndex = 33;
+            this.guna2Button2.Text = "Ginerer L\'étiquette";
+            this.guna2Button2.UseTransparentBackground = true;
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // DescreptionComboBox
+            // 
+            this.DescreptionComboBox.AutoRoundedCorners = true;
+            this.DescreptionComboBox.BackColor = System.Drawing.Color.Transparent;
+            this.DescreptionComboBox.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(216)))));
+            this.DescreptionComboBox.BorderRadius = 17;
+            this.DescreptionComboBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.DescreptionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DescreptionComboBox.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.DescreptionComboBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.DescreptionComboBox.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.DescreptionComboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.DescreptionComboBox.ItemHeight = 30;
+            this.DescreptionComboBox.Location = new System.Drawing.Point(338, 174);
+            this.DescreptionComboBox.Name = "DescreptionComboBox";
+            this.DescreptionComboBox.Size = new System.Drawing.Size(272, 36);
+            this.DescreptionComboBox.TabIndex = 34;
+            this.DescreptionComboBox.SelectedIndexChanged += new System.EventHandler(this.DescreptionComboBox_SelectedIndexChanged);
+            // 
+            // NumeroInventaireCB
+            // 
+            this.NumeroInventaireCB.AutoRoundedCorners = true;
+            this.NumeroInventaireCB.BackColor = System.Drawing.Color.Transparent;
+            this.NumeroInventaireCB.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(216)))));
+            this.NumeroInventaireCB.BorderRadius = 17;
+            this.NumeroInventaireCB.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.NumeroInventaireCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.NumeroInventaireCB.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.NumeroInventaireCB.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.NumeroInventaireCB.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.NumeroInventaireCB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.NumeroInventaireCB.ItemHeight = 30;
+            this.NumeroInventaireCB.Location = new System.Drawing.Point(338, 244);
+            this.NumeroInventaireCB.Name = "NumeroInventaireCB";
+            this.NumeroInventaireCB.Size = new System.Drawing.Size(272, 36);
+            this.NumeroInventaireCB.TabIndex = 35;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(14, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 22);
+            this.label1.TabIndex = 45;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(14, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 22);
+            this.label2.TabIndex = 46;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(334, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 22);
+            this.label3.TabIndex = 48;
+            this.label3.Text = "Lieu : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(334, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 22);
+            this.label5.TabIndex = 49;
+            this.label5.Text = "N°Inventaire :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(334, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 22);
+            this.label6.TabIndex = 50;
+            this.label6.Text = "Bien : ";
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(39, 184);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 194);
+            this.pictureBox1.TabIndex = 47;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Etiquette
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LieuComboBox);
+            this.Controls.Add(this.NumeroInventaireCB);
+            this.Controls.Add(this.DescreptionComboBox);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2VSeparator2);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Etiquette";
+            this.Size = new System.Drawing.Size(622, 501);
+            this.Load += new System.EventHandler(this.Etiquette_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2VSeparator guna2VSeparator2;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2ComboBox LieuComboBox;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2ComboBox DescreptionComboBox;
+        private Guna.UI2.WinForms.Guna2ComboBox NumeroInventaireCB;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+    }
+}
